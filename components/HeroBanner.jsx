484a7f240fb050c34eb1d8933cdceb93607b81a6
Antony <antonyhy1999@gmail.com>
2022-10-20 19:59:36 +0800

    初次存檔案

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import { urlFor } from '../lib/client'
+// 39:14
+const HeroBanner = ({banner}) => {
+  return (
+    <div className='hero-banner-container'>
+        <div>
+            <p className='beats-solo'>{banner[0].smallText? banner[0].smallText: "none"}</p>
+            <h3>{banner[0].midText}</h3>
+            <h1>{banner[0].largeText}</h1>
+            <img src={urlFor(banner[0].image).url()} alt='heads-phone' className='hero-banner-image'/>
+
+            <div>
+              <Link href={`/product/${banner.product}`}>
+                <button className='button'>Button</button>
+              </Link>
+              <div className='desc'>
+                <h5>Describetion</h5>
+                <p>descroption</p>
+              </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroBanner
+
